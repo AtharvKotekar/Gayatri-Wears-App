344cb37527e1f44f8d08393a5c170f674f14785f
AtharvKotekar <atharvkotekar7@gmail.com>
2022-05-20 21:09:10 +0530

    Home navigation Created

---
+++ app/src/main/java/com/gayatriladieswears/app/CartFragment.kt
@@ -0,0 +1,18 @@
+package com.gayatriladieswears.app
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.gayatriladieswears.app.databinding.FragmentCartBinding
+
+
+class CartFragment : Fragment() {
+    private lateinit var binding:FragmentCartBinding
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentCartBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+}
--- app/src/main/java/com/gayatriladieswears/app/HomeActivity.kt
+++ app/src/main/java/com/gayatriladieswears/app/HomeActivity.kt
@@ -2,10 +2,31 @@ package com.gayatriladieswears.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.DataBindingUtil.*
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.gayatriladieswears.app.R.layout.activity_home
+import com.gayatriladieswears.app.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityHomeBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_home)
+
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment2) as NavHostFragment
+        navController = navHostFragment.navController
+
+        setupWithNavController(binding.bottomNav,navController)
+
+
+
+
     }
 }
